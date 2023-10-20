fad15f76789973f88312f14d49fb4411aca5f2af
학원 <test@test.com>
2023-10-20 21:47:58 +0900

    JPA

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,15 +1,22 @@
 package com.example.lesson04.bo;
 
+import java.time.ZonedDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.domain.Student;
 import com.example.lesson04.mapper.StudentMapper;
+import com.example.lesson07.entity.StudentEntity;
+import com.example.lesson07.repository.StudentRepository;
 
 @Service
 public class StudentBO {
 	@Autowired
-	private StudentMapper studentMapper;
+	private StudentMapper studentMapper; // mybatis
+	
+	@Autowired
+	private StudentRepository studentRepository;
 	
 	public void addStudent(Student student) {
 		studentMapper.insertStudent(student);
@@ -18,4 +25,19 @@ public class StudentBO {
 	public void getStudentById(int id) {
 		studentMapper.loadStudentById(id);
 	}
+	
+	// jpa ------------------
+	// input: 파라미터들 output: StudentEntity
+	public StudentEntity addStudent(String name, String phoneNumber, String email, String dreamJob) {
+		StudentEntity student = StudentEntity.builder()
+				.name(name)
+				.phoneNumber(phoneNumber)
+				.email(email)
+				.dreamJob(dreamJob)
+				.createdAt(ZonedDateTime.now()) // @UpdateTimestamp 있으면 생략 가능
+				.build();
+		return studentRepository.save(student); // insert 문을 만든다.
+		
+	}
+	
 }
---
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -0,0 +1,33 @@
+package com.example.lesson07;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson04.bo.StudentBO;
+import com.example.lesson07.entity.StudentEntity;
+
+@RequestMapping("/lesson07/ex01")
+@RestController
+public class Lesson07Ex01RestController {
+	@Autowired
+	private StudentBO studentBO;
+	
+	// C U D
+	
+	// C : create insert
+	@GetMapping("/1")
+	public StudentEntity create() {
+		String name ="정재우";
+		String phoneNumber = "010-1111-2222";
+		String email = "jju@naver.com";
+		String dreamJob = "개발자";
+	
+	// 지금 들어간 id 값도 바로 꺼낼 수 있다. getId();
+	return studentBO.addStudent(name, phoneNumber, email, dreamJob);
+		
+	}
+	
+	@
+}
